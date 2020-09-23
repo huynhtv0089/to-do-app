1aca0ea28e2da7bf1439b2f127ceb2041bc0f1ca
huynhtv0089 <huynhtv0089@yahoo.com>
2020-09-23 17:04:09 +0700

    Update react-one-file.js

--- react-one-file.js
+++ react-one-file.js
@@ -90,3 +90,191 @@ class FormUser extends React.Component {
 }
 
 ReactDOM.render(<FormUser />, document.getElementById('root'));
+
+======================================================================================================================
+[Action/index.js]
+  
+export const increaseNumber = () => ({
+    type: 'INCREASE'
+})
+  
+export const decreaseNumber = () => ({
+    type: 'DECREASE'
+})
+
+export const setNumber = (value) => ({
+    type: 'SET_NUMBER',
+    number: value
+})
+---------------------------------------------------------------------------------------------------------------------
+[Container/App.js]
+
+import React from 'react';
+import Header from './Header';
+import Section from './Section';
+import Footer from './Footer';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <>
+        <Header />
+        <Section />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
+---------------------------------------------------------------------------------------------------------------------
+[Container/Footer.js]
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { decreaseNumber } from './../Action'
+
+function Footer(props) {
+    return (
+      <footer>
+        <p>This is area footer [{props.numberProps}]</p>
+        <button onClick={props.onDecrease}>Decrease</button>
+      </footer>
+    );
+}
+
+const mapStateToProps = (state) => ({
+  numberProps: state.NumberReducer.number
+})
+const mapDispatchToProps = (dispatch) => ({
+  onDecrease: () => dispatch(decreaseNumber())
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Footer)
+---------------------------------------------------------------------------------------------------------------------
+[Container/Header.js]
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { increaseNumber } from './../Action'
+
+function Header(props) {
+    return (
+        <header>
+            <p>This is area header [{props.numberProps}]</p>
+            <button onClick={props.onIncrease}>Increase</button>
+        </header>
+    );
+}
+
+const mapStateToProps = (state) => ({
+    numberProps: state.NumberReducer.number
+})
+const mapDispatchToProps = (dispatch) => ({
+    onIncrease: () => dispatch(increaseNumber()),
+})
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Header)
+---------------------------------------------------------------------------------------------------------------------
+[Container/Section.js]
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { setNumber } from './../Action'
+
+class Section extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        }
+        this.onChangeNumber = this.onChangeNumber.bind(this);
+    }
+
+    onChangeNumber(e) {
+        this.setState({
+            number: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <section>
+                <p>This is area Section [{this.props.numberProps}]</p>
+                <div>
+                    <input type="number" onChange={this.onChangeNumber} value={this.state.number} />
+                    <button onClick={(e) => this.props.passNumber(this.state.number, e)}>Submit</button>
+                </div>
+            </section>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    numberProps: state.NumberReducer.number
+})
+const mapDispatchToProps = (dispatch) => ({
+    passNumber: (number) => dispatch(setNumber(number)),
+})
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Section)
+---------------------------------------------------------------------------------------------------------------------
+[Reducer/numberReducer.js]
+
+function numberReducer(state = {number: 0}, action) {
+    switch (action.type) {
+      case 'INCREASE':
+        return {
+          number: ++state.number
+        }
+      case 'DECREASE':
+        return {
+          number: --state.number
+        }
+      case 'SET_NUMBER':
+        console.log(action.number);
+        return {
+          number: action.number
+        }
+      default:
+        return state
+    }  
+}
+
+export default numberReducer;
+---------------------------------------------------------------------------------------------------------------------
+[Reducer/RootReducer.js]
+
+import { combineReducers } from 'redux';
+import NumberReducer from './NumberReducer';
+
+export default combineReducers({
+    NumberReducer
+})
+---------------------------------------------------------------------------------------------------------------------
+[index.js]
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import RootReducer from './Reducer/RootReducer';
+import App from './Container/App';
+
+const store = createStore(RootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
+
